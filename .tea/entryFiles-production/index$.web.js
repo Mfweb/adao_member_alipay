82177608de737edf8760a89745ccce1fc7a64da2
Mfweb <h@mfweb.pw>
2018-10-05 21:32:25 +0800

    增加关于页面

--- .tea/entryFiles-production/index$.web.js
+++ .tea/entryFiles-production/index$.web.js
@@ -2,3 +2,4 @@ require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 require('../../pages/index/index');
 require('../../pages/userMember/userMember');
+require('../../pages/about/about');
--- .tea/entryFiles-production/index$.worker.js
+++ .tea/entryFiles-production/index$.worker.js
@@ -5,5 +5,6 @@ function success() {
 require('../..//app');
 require('../../pages/index/index');
 require('../../pages/userMember/userMember');
+require('../../pages/about/about');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
